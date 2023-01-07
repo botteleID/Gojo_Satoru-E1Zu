fd4d33d6b049e31903613f4b2fa88e4acb670440
JagoanRedesign <70496030+JagoanRedesign@users.noreply.github.com>
2023-01-08 05:41:35 +0700

    Update fun.py

--- Powers/plugins/fun.py
+++ Powers/plugins/fun.py
@@ -42,7 +42,9 @@ async def fun_shout(_, m: Message):
 
 
 
-@Gojo.on_message_handler(content_types=["text"])
+
+@Gojo.message_handler(func=lambda message: True)
+
 async def send_text(_, m: Message):
     if len(m.text.lower()) == "hello":
         await m.reply_text(text="ini hello")        
