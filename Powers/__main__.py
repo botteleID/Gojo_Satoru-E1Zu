b4f928c5c7c79af38ef20d386827ee07e6c65853
JagoanRedesign <70496030+JagoanRedesign@users.noreply.github.com>
2023-01-07 15:30:57 +0700

    Update __main__.py

--- Powers/__main__.py
+++ Powers/__main__.py
@@ -1,4 +1,20 @@
 from Powers.bot_class import Gojo
 
+import html
+import os
+import json
+import importlib
+import time
+import re
+import sys
+import traceback
+
+from Powers import (
+    app,
+    telethn,
+    pbot,
+    updater,
+)
+
 if __name__ == "__main__":
     Gojo().run()
