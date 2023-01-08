29d9ee7b296cab6687f0e9d310379eccf4e78524
JagoanRedesign <70496030+JagoanRedesign@users.noreply.github.com>
2023-01-08 09:07:12 +0700

    Update couples.py

--- Powers/plugins/couples.py
+++ Powers/plugins/couples.py
@@ -5,6 +5,7 @@ from Powers import LOGGER
 from Powers.bot_class import Gojo
 
 from Powers.utils.errors import capture_err
+from Powers.database.users_db import get_couple, save_couple
 
 from pyrogram import filters
 import random
