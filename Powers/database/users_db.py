d5b76aa73dbbc7d1a87f6efea9e8bc436fdd1b84
JagoanRedesign <70496030+JagoanRedesign@users.noreply.github.com>
2023-01-08 09:10:07 +0700

    Update users_db.py

--- Powers/database/users_db.py
+++ Powers/database/users_db.py
@@ -48,14 +48,13 @@ class Users(MongoDB):
 
 
 
-async def get_couple(chat_id: int, date: str):
+def get_couple(chat_id: int, date: str):
     lovers = await _get_lovers(chat_id)
     if date in lovers:
         return lovers[date]
     return False
 
-
-async def save_couple(chat_id: int, date: str, couple: dict):
+def save_couple(chat_id: int, date: str, couple: dict):
     lovers = await _get_lovers(chat_id)
     lovers[date] = couple
     coupledb.update_one(
