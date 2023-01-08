f3a13864be607dabb3deaf46f9a26c1203dbedc5
JagoanRedesign <70496030+JagoanRedesign@users.noreply.github.com>
2023-01-08 09:17:29 +0700

    Update users_db.py

--- Powers/database/users_db.py
+++ Powers/database/users_db.py
@@ -40,26 +40,6 @@ class Users(MongoDB):
         with INSERTION_LOCK:
             return self.user_info
    
- 
-def get_couple(chat_id: int, date: str):
-    lovers = await _get_lovers(chat_id)
-    if date in lovers:
-        return lovers[date]
-    return False
-
-def save_couple(chat_id: int, date: str, couple: dict):
-    lovers = await _get_lovers(chat_id)
-    lovers[date] = couple
-    coupledb.update_one(
-        {"chat_id": chat_id},
-        {"$set": {"couple": lovers}},
-        upsert=True,
-    )
-
-
-
-
-
 
 
     @staticmethod
